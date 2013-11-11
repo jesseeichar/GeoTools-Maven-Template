c21726e5847e9eabbae5fe4b79ce11202233251e
Jesse Eichar <jesse.eichar@camptocamp.com>
2013-11-11 09:58:20 +0100

    added protractor test support

--- src/main/webapp/static/js/square.js
+++ src/main/webapp/static/js/square.js
@@ -1,12 +1,14 @@
-/**
- * @param {number} n N.
- * @return {number} N squared.
- */
-function square(n){
-    return n*n;
-};;;;;;;
+(function() {
+    "use strict";
+    function square(n){
+        return n*n;
+    }
 
-window.console.log(square(5));
-
-//var y = 1024 * 1024 * 1024 * 2 >> 2;
-//window.console.log(y);
+    function displaySquare(elemId, n) {
+        var elem = document['getElementById'](elemId);
+        elem['innerText'] = n + ' * ' + n + '=' + square(n);
+    }
+    window['closureExportedFunctions'] = {
+        'square': displaySquare
+    };
+})();
